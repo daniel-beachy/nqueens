6a2b51ec95f8d77ed4b70af5141ebc1e8d40e3ba
Daniel Beachy <dbeachy20@georgefox.edu>
2024-04-15 20:40:14 -0700

    remove reference to manifest.json

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,7 +6,6 @@ function Landing() {
     <div className="landing">
       <h1>My Portfolio</h1>
       <p>Welcome to my professional portfolio website!</p>
-      {/* Consider adding links to individual projects or sections */}
       <Link to="/nqueens" className="nqueens-button">
         Try N-Queens Solver
       </Link>
