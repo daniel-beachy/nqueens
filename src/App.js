b91251f84b80b65edf672a560d76bcb967e37b42
Daniel Beachy <dbeachy20@georgefox.edu>
2024-04-15 21:02:34 -0700

    switch to HashRouter for static GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
 import NQueens from "./components/n-queens/NQueens";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Landing />} />{" "}
-          <Route path="/nqueens" element={<NQueens />} />{" "}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Landing />} />{" "}
+        <Route path="/nqueens" element={<NQueens />} />{" "}
+      </Routes>
+    </div>
   );
 }
 
