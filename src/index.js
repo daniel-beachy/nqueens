680a266289f8ceb7ed929511227e1551b9e5f5c2
Daniel Beachy <dbeachy20@georgefox.edu>
2024-04-20 17:55:19 -0700

    rework base App

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import "./custom.scss";
+import NQueens from "./components/NQueens";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter hashType="noslash">
-      <App />
-    </HashRouter>
+    <NQueens />
   </React.StrictMode>
 );
